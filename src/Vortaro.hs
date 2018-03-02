965eb7ce54a852632bdb269dc3b617a3a02c7acc
mike <michaelvessia@gmail.com>
2018-03-01 23:57:05 -0500

    Use more robust types

--- src/Vortaro.hs
+++ src/Vortaro.hs
@@ -5,32 +5,34 @@ import Data.Char (toLower)
 
 -- An English word
 type EnWord = String
--- The Esperanto dictionary file
-type EspDic = String
 
-data EoWord = EoWord String
+-- An Esperanto word, found on the LHS of a dictionary entry
+data EoWord = EoWord String deriving (Show, Eq)
 
-data Definition = Definition String
+-- A definition of an Esperanto word, found on the RHS of a dictionary entry
+data Definition = Definition String deriving (Show, Eq)
 
-data Entry = Entry EoWord Definition
+-- Product type representing a single line in the dictionary
+data Entry = Entry EoWord Definition deriving (Show, Eq)
 
-translate :: EnWord -> EspDic -> IO ()
-translate word espdic = do
-    if translations == [] then putStrLn "No translation found" else mapM_ putStrLn translations
-        where translations = (searchForWord (makeLowerCase word) espdic)
+translate :: EnWord -> String -> IO ()
+translate word rawDic = do
+    if translations == Nothing then putStrLn "No translation found" else mapM_ (putStrLn . show) translations
+        where translations = searchForWord (makeLowerCase word) (mkEntries (format rawDic))
 
 -- Return lines in the espdic for which the given EnWord can be found
-searchForWord :: EnWord -> EspDic -> [String]
-searchForWord word espdic = filter (isInfixOf word) (format espdic)
+searchForWord :: EnWord -> [Entry] -> Maybe [Entry]
+searchForWord word espdic
+    | results == [] = Nothing
+    | otherwise = Just results
+        where results = filter (definitionContainsWord word) espdic
 
-format :: EspDic -> [String]
+definitionContainsWord :: EnWord -> Entry -> Bool
+definitionContainsWord word (Entry _ (Definition def)) = isInfixOf word def
+
+-- Format the EspDic input String prior to its transformation into the real type
+format :: String -> [String]
 format = tail . lines . makeLowerCase . stripQuotes
-
-makeLowerCase :: String -> String
-makeLowerCase = map toLower
-
-stripQuotes :: String -> String
-stripQuotes = filter (/='"')
 
 -- Dictionary entries are split by a :
 -- Construct an Entry for a given line
@@ -50,3 +52,8 @@ split (x:xs) delim
     | otherwise = (x : head rest) : tail rest
     where rest = split xs delim
 
+makeLowerCase :: String -> String
+makeLowerCase = map toLower
+
+stripQuotes :: String -> String
+stripQuotes = filter (/='"')

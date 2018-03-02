3594a43142dd9f4a68cdb54bb1ecfdd3000d163e
mike <michaelvessia@gmail.com>
2018-03-01 22:55:25 -0500

    Printing unicode correctly

--- src/Vortaro.hs
+++ src/Vortaro.hs
@@ -2,6 +2,7 @@ module Vortaro where
 
 import Data.List (isInfixOf)
 import Data.Char (toLower)
+import qualified Data.Text as T
 
 -- An English word
 type EnWord = String
@@ -10,12 +11,18 @@ type EspDic = String
 
 translate :: EnWord -> EspDic -> IO ()
 translate word espdic = do
-    if translations == [] then putStrLn "No translation found" else mapM_ print translations
+    if translations == [] then putStrLn "No translation found" else mapM_ putStrLn translations
         where translations = (searchForWord (makeLowerCase word) espdic)
 
 -- Return lines in the espdic for which the given EnWord can be found
 searchForWord :: EnWord -> EspDic -> [String]
-searchForWord word espdic = filter (isInfixOf word) (tail . lines . makeLowerCase $ espdic)
+searchForWord word espdic = filter (isInfixOf word) (format espdic)
+
+format :: EspDic -> [String]
+format = tail . lines . makeLowerCase . stripQuotes
 
 makeLowerCase :: String -> String
 makeLowerCase = map toLower
+
+stripQuotes :: String -> String
+stripQuotes = filter (/='"')

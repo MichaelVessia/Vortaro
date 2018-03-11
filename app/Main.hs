d9fb5e17a2f1b9b96600ede74aa72dfa933682ac
mike <michaelvessia@gmail.com>
2018-03-10 21:47:02 -0500

    User can now press Q to quit

--- app/Main.hs
+++ app/Main.hs
@@ -3,28 +3,23 @@ module Main where
 
 import Vortaro
 import System.IO
+import System.Exit
 import Data.Text
 import Data.Text.IO as TIO
 
 doTranslate :: IO ()
 doTranslate = do
-    TIO.putStrLn "\n\n"
-    TIO.putStrLn "Please enter a word"
+    TIO.putStrLn "\n"
+    TIO.putStrLn "Please enter a word. Enter Q to quit."
     word <- TIO.getLine
     withFile "espdic/espdic.txt" ReadMode $ \fileHandle -> do
         contents <- TIO.hGetContents fileHandle
-        translate word contents
+        case word of
+          "Q" -> exitWith ExitSuccess
+          otherwise -> translate word contents
         main
 
 main :: IO ()
-main = do
-  TIO.putStrLn "\n\n"
-  TIO.putStrLn "What would you like to do?"
-  TIO.putStrLn "1 Translate a word"
-  TIO.putStrLn "2 Quit"
-  action <- TIO.getLine
-  case action of
-    "1" -> doTranslate
-    "2" -> return ()
-    otherwise -> main
+main = doTranslate
+
 
